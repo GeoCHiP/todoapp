48b06d531b982b7d50c4b14ad88919b5d352ac59
GeoCHiP <x.GeoCHiP.x@yandex.ru>
2022-07-31 15:06:08 +0300

    fix: use an environment variable for SECRET_KEY if it exists

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@80fy%d4hu$d&k_=a)fpg$gn4#a-)r(dyo(72e7bul3a5@wbf@'
+SECRET_KEY = os.environ['SECRET_KEY'] or 'django-insecure-@80fy%d4hu$d&k_=a)fpg$gn4#a-)r(dyo(72e7bul3a5@wbf@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
