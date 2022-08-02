1f769074ad4da685993bb984ca1dbe87d91235da
GeoCHiP <x.GeoCHiP.x@yandex.ru>
2022-08-02 18:00:40 +0300

    fix: use os.environ.get instead of os.environ[]

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY'] or 'django-insecure-@80fy%d4hu$d&k_=a)fpg$gn4#a-)r(dyo(72e7bul3a5@wbf@'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'django-insecure-@80fy%d4hu$d&k_=a)fpg$gn4#a-)r(dyo(72e7bul3a5@wbf@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
